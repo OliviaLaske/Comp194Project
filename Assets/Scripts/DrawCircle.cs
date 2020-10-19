8a28bd9368ffc5351ade9035b4063de511bda6f2
OliviaLaske <olaske@macalester.edu>
2020-10-19 15:23:57 -0500

    Tried new pencil script.

---
+++ Assets/Scripts/DrawCircle.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCircle : MonoBehaviour
+{
+    public OVRInput.Controller controller;
+    private LineRenderer lineRenderer;
+    private GameObject drawPoint;
+    private GameObject lineRendererParent;
+
+    private bool drawing;
+    
+    public Transform RightHandAnchor; // Potentially Transform class?
+   // public GameObject pencil;
+
+   Vector3 startPoint;
+
+    void start()
+    {
+        lineRenderer = RightHandAnchor.GetComponent<LineRenderer>();
+    }
+    private void Drawing()
+    {
+        if (Input.GetAxis("Oculus_CrossPlatform_PrimaryHandTrigger")>0.2f)
+        {
+           // Drawing();
+            drawing = true;
+            lineRendererParent = new GameObject();
+            lineRendererParent.transform.position = transform.position;
+            lineRenderer = lineRendererParent.AddComponent<LineRenderer>();
+            lineRenderer.startWidth = 0.01f;
+            
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, 1.0f))
+            {
+                if(hit.transform.tag == "Paper")
+                {
+                    startPoint = hit.point;
+                    lineRenderer.SetPosition(0, startPoint);
+                    if(drawing == true)
+                    {
+                        lineRenderer.SetPosition(1, hit.point);
+                    }
+                }
+            }
+            
+           // lineRenderer.SetPosition(0, drawPoint.transform.position);
+            // lineRenderer.SetPosition(1, hit.point); // drawPoint.transform.position);
+        }
+            // if (drawing == true)
+            // {
+                // lineRenderer.SetPosition(lineRenderer.positionCount++, drawPoint.transform.position);
+            // }
+
+            // if(drawing == true)
+            // {
+                // lineRenderer.SetPosition(1, hit.point);
+            // }
+    }
+
+}
---
+++ Assets/Scripts/DrawCircle1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCircle1 : MonoBehaviour
+{
+    public GameObject graphite;
+    Vector3 graphitePoint = new Vector3(0f, 0f, 0.1f);
+    Vector3 pencilPosition;
+    Vector3 drawPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Oculus_CrossPlatform_SecondaryIndexTrigger") > 0.2f)
+        {
+            pencilPosition = transform.position;
+            drawPoint = pencilPosition + graphitePoint;
+            Instantiate(graphite, drawPoint, transform.rotation);
+        }
+
+        /*
+        if(Input.GetAxis("Oculus_CrossPlatform_SecondaryIndexTrigger") > 0.2f)
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.transform.tag == "Paper")
+                {
+                    point = new Vector3(transform.position.x, transform.position.y, transform.position.z) - new Vector3(0f, 0f, -0.083f);
+                    Instantiate(graphite, point, transform.rotation);
+                }
+            }
+        }
+        */
+    }
+}
