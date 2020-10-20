f7fe9cea669a7e67f73f397e6ca315b112766b34
OliviaLaske <olaske@macalester.edu>
2020-10-19 22:38:09 -0500

    Edited DrawCircle2.

--- Assets/Scripts/DrawCircle2.cs
+++ Assets/Scripts/DrawCircle2.cs
@@ -10,6 +10,8 @@ public class DrawCircle2 : MonoBehaviour
     public AudioClip incorrectCircle;
     public AudioClip pencilPlacementInstructions;
     bool isDrawing = false;
+    bool isCirclePerfect = false;
+    int counter = 0;
 
     IEnumerator WaitForSeconds()
     {
@@ -20,6 +22,33 @@ public class DrawCircle2 : MonoBehaviour
     {
         yield return new WaitForSeconds(6.312f);
     }
+
+    void DrawCircle()
+    {
+        // for(int i = 0; i < 2; i++)
+        while(isCirclePerfect == false)
+        {
+            if(Input.GetAxis("Oculus_CrossPlatform_SecondaryIndexTrigger") > 0.2f)
+            {
+                graphite.GetComponent<TrailRenderer>().enabled = true;
+                isDrawing = true;
+            }
+            else
+            {
+                StartCoroutine(WaitForSeconds());
+                graphite.GetComponent<TrailRenderer>().Clear();
+                graphite.GetComponent<TrailRenderer>().enabled = false;
+                if(isDrawing == true)
+                {
+                    audioSource.clip = incorrectCircle;
+                    audioSource.Play(0);
+                    counter = counter + 1;
+                }
+                isDrawing = false;
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +59,7 @@ public class DrawCircle2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         for(int i = 0; i < 10; i++)
         {
             if(Input.GetAxis("Oculus_CrossPlatform_SecondaryIndexTrigger") > 0.2f)
@@ -50,12 +80,24 @@ public class DrawCircle2 : MonoBehaviour
                 isDrawing = false;
             }
         }
+        */
+
+        // Calls method to complete the task.
+        DrawCircle();
 
         // When the task is to be completed.
-        circle.SetActive(true);
-        audioSource.clip = pencilPlacementInstructions;
-        audioSource.Play(0);
-        StartCoroutine(WaitForSeconds1());
-        circle.SetActive(false);
+        if(counter == 2)
+        {
+            isCirclePerfect = true;
+        }
+
+        if(isCirclePerfect == true)
+        {
+            circle.SetActive(true);
+            audioSource.clip = pencilPlacementInstructions;
+            audioSource.Play(0);
+            StartCoroutine(WaitForSeconds1());
+            circle.SetActive(false);
+        }
     }
 }
