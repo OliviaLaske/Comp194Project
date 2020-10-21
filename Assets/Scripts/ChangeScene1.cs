a57fde64abbca4612f3c90da59aebb4d835a4c72
OliviaLaske <olaske@macalester.edu>
2020-10-21 08:34:20 -0500

    Readded introduction scene.

---
+++ Assets/Scripts/ChangeScene1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            SceneManager.LoadScene("Classroom", LoadSceneMode.Single);
+        }
+    }
+}
