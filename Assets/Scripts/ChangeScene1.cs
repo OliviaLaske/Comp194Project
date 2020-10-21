6d4be1d384bdd228d17d55673e9163b34aeae643
OliviaLaske <olaske@macalester.edu>
2020-10-21 16:51:25 -0500

    Edited scene manager.

--- Assets/Scripts/ChangeScene1.cs
+++ Assets/Scripts/ChangeScene1.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ChangeScene1 : MonoBehaviour
 {
     public int index;
+    public TextMeshPro press;
+
+    IEnumerator WaitForSeconds()
+    {
+        yield return new WaitForSeconds(27.5f);
+        press.text = "Press B to continue";
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-    
+        press.text = " ";
     }
 
     // Update is called once per frame
